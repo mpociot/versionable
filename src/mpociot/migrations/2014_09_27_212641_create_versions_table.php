3696ef19dfa012558c84f4afa9ec2a3b62fc3a2c
Marcel Pociot <m.pociot@gmail.com>
2014-09-28 00:29:39 +0200

    Initial commit

---
+++ src/mpociot/migrations/2014_09_27_212641_create_versions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVersionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('versions', function(Blueprint $table)
+		{
+			$table->increments('version_id');
+			$table->integer('versionable_id');
+            $table->text('versionable_type');
+			$table->integer('user_id');
+			$table->binary('model_data');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('versions');
+	}
+
+}
