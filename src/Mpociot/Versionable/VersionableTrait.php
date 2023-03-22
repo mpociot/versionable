ecfc62fc619e894ca70c511484f59b117f23e016
Danny Boy <orienta.dan@gmail.com>
2023-03-22 22:25:41 +0900

    create initial version

--- src/Mpociot/Versionable/VersionableTrait.php
+++ src/Mpociot/Versionable/VersionableTrait.php
@@ -189,6 +189,47 @@ trait VersionableTrait
         }
     }
 
+
+    /**
+     * Initialize a version on every instance of a model
+     * @return void
+     */
+    public static function initializeVersionOnAllRows()
+    {
+        foreach (self::all() as $obj) {
+            $obj->createInitialVersion();
+        }
+        return true;
+    }
+
+    /**
+     * Save a new version.
+     * @return void
+     */
+    public function createInitialVersion()
+    {
+        if(true === $this->versions->isEmpty()) {
+
+            $class                     = $this->getVersionClass();
+            $version                   = new $class();
+            $version->versionable_id   = $this->getKey();
+            $version->versionable_type = method_exists($this, 'getMorphClass') ? $this->getMorphClass() : get_class($this);
+            $version->user_id          = $this->getAuthUserId();
+            
+            $versionedHiddenFields = $this->versionedHiddenFields ?? [];
+            $this->makeVisible($versionedHiddenFields);
+            $version->model_data       = serialize($this->attributesToArray());
+            $this->makeHidden($versionedHiddenFields);
+
+            if (!empty( $this->reason )) {
+                $version->reason = $this->reason;
+            }
+
+            $version->save();
+        }
+    }
+
+
     /**
      * Delete old versions of this model when they reach a specific count.
      * 
